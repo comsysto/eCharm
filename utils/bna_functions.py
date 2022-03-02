d4d4ff2ab13e4114f3e820bf4fd3b6803eebb996
Markus Steger <m.steger@reply.de>
2022-03-02 07:38:14 +0100

    format

--- utils/bna_functions.py
+++ utils/bna_functions.py
@@ -7,9 +7,7 @@ def check_coordinates(coords: float) -> float:
     if isinstance(coords, str):
         log.warn(f"Coords are string: {coords} will be transformed!")
         coords = float(
-            "".join(
-                [s for s in coords.replace(",", ".") if (s.isdigit()) | (s == ".")]
-            )
+            "".join([s for s in coords.replace(",", ".") if (s.isdigit()) | (s == ".")])
         )
     if not isinstance(coords, Number):
         raise ValueError("Coordinatess could not be read propery!")
