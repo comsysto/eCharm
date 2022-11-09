9d6624b5af56cc04d7c2bdb72a46793f63dad18a
Markus Steger <m.steger@reply.de>
2022-11-09 07:57:36 +0100

    format

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import os
 import pathlib
 
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-from pipelines._merger import StationMerger
 from settings import db_uri
 
 if __name__ == "__main__":
@@ -13,26 +13,34 @@ if __name__ == "__main__":
     config: configparser = configparser.RawConfigParser()
     config.read(os.path.join(os.path.join(current_dir, "config", "config.ini")))
 
-    # bna: BnaPipeline = BnaPipeline(
-    #     config=config,
-    #     session=sessionmaker(bind=(create_engine(db_uri, echo=True)))(),
-    #     offline=True,
-    # )
-    # bna.run()
-    # osm: OsmPipeline = OsmPipeline(
-    #     config=config,
-    #     session=sessionmaker(bind=(create_engine(db_uri, echo=True)))(),
-    #     offline=True,
-    # )
-    # osm.run()
+    from pipelines._bna import BnaPipeline
 
-    # ocm: OcmPipeline = OcmPipeline(
-    #     config=config,
-    #     session=sessionmaker(bind=(create_engine(db_uri, echo=True)))(),
-    #     offline=True,
-    # )
-    # ocm.run()
+    bna: BnaPipeline = BnaPipeline(
+        config=config,
+        session=sessionmaker(bind=(create_engine(db_uri, echo=True)))(),
+        offline=True,
+    )
+    bna.run()
 
-    StationMerger(config=config, con=create_engine(db_uri, echo=True)).retrieveData()
+    from pipelines._osm import OsmPipeline
+
+    osm: OsmPipeline = OsmPipeline(
+        config=config,
+        session=sessionmaker(bind=(create_engine(db_uri, echo=True)))(),
+        offline=True,
+    )
+    osm.run()
+
+    from pipelines._ocm import OcmPipeline
+
+    ocm: OcmPipeline = OcmPipeline(
+        config=config,
+        session=sessionmaker(bind=(create_engine(db_uri, echo=True)))(),
+        offline=True,
+    )
+    ocm.run()
+
+    # from pipelines._merger import StationMerger
+    # StationMerger(config=config, con=create_engine(db_uri, echo=True)).retrieveData()
 
     print("")
