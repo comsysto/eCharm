fbfe951d2f21869f38694801fdf20a820c3865b3
Markus Steger <m.steger@reply.de>
2022-11-09 07:32:54 +0100

    cleanup

--- main.py
+++ main.py
@@ -1,44 +1,10 @@
-# import for the pipeline
 import os
 import pathlib
 
-import pandas as pd
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from tqdm import tqdm
 
-from mapping.charging import map_charging_ocm
-from mapping.stations import map_address_ocm, map_station_ocm
-from models.station import Station
 from pipelines._merger import StationMerger
-from pipelines._ocm import OcmPipeline
 from settings import db_uri
-
-
-def ocm_pipeline():
-    # Read excel file as pandas dataframe
-    df = pd.read_csv(r"data/ocm_stations.csv")
-
-    # df_mapped = map_address(df_dropped)
-    engine = create_engine(db_uri, echo=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    for index, row in tqdm(df.iterrows()):
-        mapped_station: Station = map_station_ocm(row)
-        session.add(mapped_station)
-        session.flush()
-        mapped_address = map_address_ocm(row, mapped_station.id)
-        session.add(mapped_address)
-        mapped_charging = map_charging_ocm(row, mapped_station.id)
-        session.add(mapped_charging)
-
-        try:
-            session.commit()
-        except Exception as e:
-            print(e)
-            session.rollback()
-
 
 if __name__ == "__main__":
     current_dir = os.path.join(pathlib.Path(__file__).parent.resolve())
@@ -67,9 +33,6 @@ if __name__ == "__main__":
     # )
     # ocm.run()
 
-    StationMerger(
-       config=config,
-       con=create_engine(db_uri, echo=True)
-    ).retrieveData()
+    StationMerger(config=config, con=create_engine(db_uri, echo=True)).retrieveData()
 
     print("")
--- pipelines/_merger.py
+++ pipelines/_merger.py
@@ -10,9 +10,9 @@ class StationMerger:
         self.con = con
 
     def retrieveData(self):
-        sql = 'SELECT * FROM stations'
+        sql = "SELECT * FROM stations"
         gdf = gpd.read_postgis(sql, con=self.con, geom_col="coordinates")
         gdf_dropped = gdf.drop(gdf[["date_created", "date_updated"]], axis=1)
-        out_json = os.path.join('nyc_neighborhoods.geojson')
+        out_json = os.path.join("nyc_neighborhoods.geojson")
         gdf_dropped.to_file(out_json, driver="GeoJSON")
         print("")
