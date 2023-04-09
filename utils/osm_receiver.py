0961c9ab28ae25a87fc0e08798f2e51f8f0b6e08
Stefan Pernpaintner <stefan.pernpaintner@gmail.com>
2023-04-09 13:13:47 +0200

    change osm area name from Italy to Italia

--- utils/osm_receiver.py
+++ utils/osm_receiver.py
@@ -9,7 +9,7 @@ def get_osm_data(country_code: str, tmp_data_path):
         "DE": "Deutschland",
         "FR": "France métropolitaine",
         "GB": "United Kingdom",
-        "IT": "Italy"
+        "IT": "Italia"
     }
 
     if country_code not in country_code_to_area:
