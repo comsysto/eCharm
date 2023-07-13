560f87aeba54285c464f5f68ad30e1522bd3a969
Lars Haferkamp <>
2023-07-13 14:46:06 +0200

    Set user-agent for BNA download

--- charging_stations_pipelines/shared.py
+++ charging_stations_pipelines/shared.py
@@ -36,7 +36,11 @@ def string_to_bool(bool_string: str) -> bool:
 
 
 def download_file(url, target_file):
-    resp = requests.get(url)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                      'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'
+    }
+    resp = requests.get(url, headers=headers)
     output = open(target_file, "wb")
     output.write(resp.content)
     output.close()
