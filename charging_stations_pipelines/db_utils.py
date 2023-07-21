8a0de5b521e9c1f53fdb05fa45f3330d9009a499
Lars Haferkamp <>
2023-07-21 16:42:19 +0200

    Added option to delete relevant data before running import or merge

---
+++ charging_stations_pipelines/db_utils.py
@@ -0,0 +1,31 @@
+from sqlalchemy import delete, text, update
+from sqlalchemy.orm import Session
+
+from charging_stations_pipelines import settings
+from charging_stations_pipelines.models import address, charging, station
+
+
+def delete_alembic_version(session: Session):
+    session.execute(text(f"DELETE FROM {settings.db_schema}.alembic_version"))
+
+
+def delete_all_data(session: Session):
+    session.execute(delete(station.MergedStationSource))
+    session.execute(delete(address.Address))
+    session.execute(delete(charging.Charging))
+    session.execute(delete(station.Station))
+    session.commit()
+    session.close()
+
+
+def delete_all_merged_data(session: Session):
+    session.execute(delete(station.MergedStationSource))
+    session.execute(delete(address.Address).where(address.Address.is_merged==True))
+    session.execute(delete(charging.Charging).where(charging.Charging.is_merged==True))
+    session.execute(delete(station.Station).where(station.Station.is_merged==True))
+    session.execute(update(station.Station).values(merge_status=None))
+    session.commit()
+    session.close()
+
+
+
--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from charging_stations_pipelines.settings import db_uri
 from charging_stations_pipelines.shared import reject_if, config, string_to_bool
 from charging_stations_pipelines.stations_data_export import stations_data_export
 from charging_stations_pipelines import settings
+from charging_stations_pipelines import db_utils
 from testing import testdata
 
 logger = logging.getLogger("charging_stations_pipelines.main")
@@ -26,9 +27,12 @@ class CommandLineArguments:
     def __init__(self, argv) -> None:
         super().__init__()
         self.argv = argv
+        # explicitly set defaults
+        self.delete_data = False
+        self.online = False
 
         try:
-            opts, args = getopt.getopt(argv[1:], "ht:c:o:", ["help", "tasks=", "countries=", "online="])
+            opts, args = getopt.getopt(argv[1:], "ht:c:o:", ["help", "tasks=", "countries=", "online=", "delete_data="])
         except Exception:
             logger.exception("Could not parse arguments")
             raise
@@ -42,10 +46,13 @@ class CommandLineArguments:
                 self.countries = [c.upper() for c in arg.split(",")]
             elif opt in ("-o", "--online"):
                 self.online = string_to_bool(arg)
+            elif opt in ("-d", "--delete_data"):
+                self.delete_data = string_to_bool(arg)
         self.validate()
 
     def print_help(self):
-        arg_help = "{0} --tasks=<task1,task2> --countries=<country1,country2> --online=<online>".format(self.argv[0])
+        arg_help = "{0} --tasks=<task1,task2> --countries=<country1,country2> --online=<true/false> " \
+                   "--delete_data=<true/false>".format(self.argv[0])
         print(arg_help)
         print("Example: python main.py --countries=de,it --tasks=import --online=true")
         sys.exit(2)
@@ -65,11 +72,14 @@ def get_db_engine(**kwargs):
     connect_args = {"options": f"-csearch_path={settings.db_schema},public"}
     return create_engine(name_or_url=db_uri, connect_args=connect_args, **kwargs)
 
-def run_import(countries, online):
 
-    db_session = sessionmaker(bind=get_db_engine())()
+def run_import(countries, online: bool, delete_data: bool):
+
+    if delete_data:
+        db_utils.delete_all_data(sessionmaker(bind=get_db_engine())())
 
     for country in countries:
+        db_session = sessionmaker(bind=get_db_engine())()
         gov_pipeline = pipeline_factory(db_session, country, online)
         gov_pipeline.run()
 
@@ -80,8 +90,13 @@ def run_import(countries, online):
         ocm.run()
 
 
-def run_merge(countries):
+def run_merge(countries, delete_data: bool):
     engine = get_db_engine(pool_pre_ping=True)
+
+    if delete_data:
+        print("deleting merged data ... ---> seems it's not writing :(")
+        db_utils.delete_all_merged_data(sessionmaker(bind=engine)())
+
     for country in countries:
         merger: StationMerger = StationMerger(country_code=country, config=config, db_engine=engine)
         merger.run()
@@ -98,10 +113,11 @@ if __name__ == "__main__":
     for task in command_line_arguments.tasks:
         logger.debug("Running task " + task)
         if task == "import":
-            run_import(command_line_arguments.countries, command_line_arguments.online)
+            run_import(command_line_arguments.countries, command_line_arguments.online,
+                       command_line_arguments.delete_data)
 
         if task == "merge":
-            run_merge(command_line_arguments.countries)
+            run_merge(command_line_arguments.countries, command_line_arguments.delete_data)
 
         if task == "testdata":
             testdata.run()
