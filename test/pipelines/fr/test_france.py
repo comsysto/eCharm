417c962f0006e1687491e45fe9366e2818c1d9e5
pernpas <45739253+pernpas@users.noreply.github.com>
2023-08-04 11:15:53 +0200

    activate github actions (#45)
    * activate github actions

--- test/pipelines/fr/test_france.py
+++ test/pipelines/fr/test_france.py
@@ -3,9 +3,12 @@ from pathlib import Path
 from unittest import TestCase
 
 from charging_stations_pipelines.pipelines.fr.france import FraPipeline
+from test.shared import skip_if_github
 
 
 class TestFraPipeline(TestCase):
+
+    @skip_if_github
     def test_download_france_gov_file(self):
         temp_target = Path(__file__).parent.parent.parent.parent.joinpath("data/fra_temp.csv")
         FraPipeline.download_france_gov_file(temp_target)
